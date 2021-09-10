76b21d313fa8cfab868b91634190301fba009726
[Sofia Pareja] <[sofia.pareja@correo.ucu.edu.uy]>
2021-09-10 14:07:18 -0300

    Tests realizados

--- test/Library.Test/PersonTests.cs
+++ test/Library.Test/PersonTests.cs
@@ -4,18 +4,34 @@ using UnitTestAndDebug;
 
 namespace Tests
 {
+    [TestFixture]
     public class Tests
     {
-        [SetUp]
-        public void Setup()
+
+        [Test]
+        public void TestIdValid() // Cambiá el nombre para indicar qué estás probando
         {
-            // Insertá tu código de inicialización aquí
+            Person persona = new Person("Person", "5.267.022-1");
+            const string expectedId = "5.267.022-1";
+            persona.ID = "5.267.022-3";
+            Assert.AreEqual(expectedId,persona.ID);
         }
 
         [Test]
-        public void Test1() // Cambiá el nombre para indicar qué estás probando
+        public void TestIdNotValid()
         {
-            // Insertá tu código  de pruebaaquí
+            Person persona = new Person("Person1","5.267.022-9");
+            const string expectedId = null;
+            Assert.AreEqual(expectedId, persona.ID);
+        }
+
+        [Test]
+        public void TestNameNotValid()
+        {
+            Person persona = new Person("","5.267.022-1");
+            const string expectedName = null;
+            Assert.AreEqual(expectedName,persona.Name);
         }
     }
+
 }
